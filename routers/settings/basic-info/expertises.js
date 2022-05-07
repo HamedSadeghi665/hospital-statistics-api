51332dbf0b2ee27fefc1cdadd5f12310c1f9f2dd
HamedSadeghi665 <hamed.sadeghi.ir@gmail.com>
2022-05-07 05:01:32 +0430

    Chage org folder to users-info & Experties Page

---
+++ routers/settings/basic-info/expertises.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const auth = require("../../../middlewares/auth");
+const router = express.Router();
+const { selectQuery } = require("../../../startup/db");
+
+router.get("/", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(`EXEC StatisticAPI.GetAllExpertises ${MemberID}`);
+
+  result = result.recordset;
+
+  if (result.length === 1 && result[0].Error)
+    return res.status(400).send(result[0]);
+
+  res.send(result);
+});
+
+router.post("/search", auth, async (req, res) => {
+  const { searchText } = req.body;
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC StatisticAPI.SearchExpertises ${MemberID}, N'${searchText}'`
+  );
+
+  result = result.recordset;
+
+  if (result.length === 1 && result[0].Error)
+    return res.status(400).send(result[0]);
+
+  res.send(result);
+});
+
+router.post("/", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC StatisticAPI.SaveExpertise ${MemberID}, N'${JSON.stringify(req.body)}'`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  res.send(result);
+});
+
+router.delete("/:recordID", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC StatisticAPI.DeleteExpertise ${MemberID}, ${req.params.recordID}`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  res.send(result);
+});
+
+module.exports = router;
--- startup/settings-routes.js
+++ startup/settings-routes.js
@@ -1,17 +1,19 @@
 const provinces = require("../routers/settings/basic-info/provinces");
 const cities = require("../routers/settings/basic-info/cities");
+const expertises = require("../routers/settings/basic-info/expertises");
 //------
 const pageAccesses = require("../routers/settings/accesses/page-accesses");
 //------
-const members = require("../routers/settings/org/members");
+const members = require("../routers/settings/users-info/members");
 //------
 
 module.exports = function (app) {
   app.use("/api/settings/basic-info/provinces", provinces);
   app.use("/api/settings/basic-info/cities", cities);
+  app.use("/api/settings/basic-info/expertises", expertises);
   //------
   app.use("/api/settings/accesses/page-accesses", pageAccesses);
   //------
-  app.use("/api/settings/org/members", members);
+  app.use("/api/settings/users-info/members", members);
   //------
 };
