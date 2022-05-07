f23dfc11ebe758ec83436e9f59ce0572b1384726
HamedSadeghi665 <hamed.sadeghi.ir@gmail.com>
2022-05-07 11:46:27 +0430

    Design Section Table Services

--- startup/settings-routes.js
+++ startup/settings-routes.js
@@ -1,6 +1,7 @@
 const provinces = require("../routers/settings/basic-info/provinces");
 const cities = require("../routers/settings/basic-info/cities");
 const expertises = require("../routers/settings/basic-info/expertises");
+const sections = require("../routers/settings/basic-info/sections");
 //------
 const pageAccesses = require("../routers/settings/accesses/page-accesses");
 //------
@@ -11,6 +12,7 @@ module.exports = function (app) {
   app.use("/api/settings/basic-info/provinces", provinces);
   app.use("/api/settings/basic-info/cities", cities);
   app.use("/api/settings/basic-info/expertises", expertises);
+  app.use("/api/settings/basic-info/sections", sections);
   //------
   app.use("/api/settings/accesses/page-accesses", pageAccesses);
   //------
