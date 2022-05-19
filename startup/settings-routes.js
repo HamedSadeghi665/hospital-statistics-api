385d8599fadfda96e76f7aeef64721c84a195dba
HamedSadeghi665 <hamed.sadeghi.ir@gmail.com>
2022-05-19 22:20:11 +0430

    Design Doctors Table Services

--- startup/settings-routes.js
+++ startup/settings-routes.js
@@ -6,6 +6,7 @@ const sections = require("../routers/settings/basic-info/sections");
 const pageAccesses = require("../routers/settings/accesses/page-accesses");
 //------
 const members = require("../routers/settings/users-info/members");
+const doctors = require("../routers/settings/users-info/doctors");
 //------
 
 module.exports = function (app) {
@@ -17,5 +18,6 @@ module.exports = function (app) {
   app.use("/api/settings/accesses/page-accesses", pageAccesses);
   //------
   app.use("/api/settings/users-info/members", members);
+  app.use("/api/settings/users-info/doctors", doctors);
   //------
 };
