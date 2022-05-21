f48bd2e18223267528583f1c689c053292af2254
HamedSadeghi665 <hamed.sadeghi.ir@gmail.com>
2022-05-21 10:52:49 +0430

    Design General Sections Table Service

--- startup/settings-routes.js
+++ startup/settings-routes.js
@@ -3,6 +3,7 @@ const cities = require("../routers/settings/basic-info/cities");
 const expertises = require("../routers/settings/basic-info/expertises");
 const sections = require("../routers/settings/basic-info/sections");
 const sectionActiveBeds = require("../routers/settings/basic-info/section-active-beds");
+const generalSections = require("../routers/settings/basic-info/general-sections");
 //------
 const pageAccesses = require("../routers/settings/accesses/page-accesses");
 //------
@@ -12,16 +13,16 @@ const sectionExperts = require("../routers/settings/users-info/section-experts")
 //------
 
 module.exports = function (app) {
+  app.use("/api/settings/users-info/members", members);
+  app.use("/api/settings/users-info/doctors", doctors);
+  app.use("/api/settings/users-info/section-experts", sectionExperts);
+  //------
   app.use("/api/settings/basic-info/provinces", provinces);
   app.use("/api/settings/basic-info/cities", cities);
   app.use("/api/settings/basic-info/expertises", expertises);
   app.use("/api/settings/basic-info/sections", sections);
   app.use("/api/settings/basic-info/section-active-beds", sectionActiveBeds);
+  app.use("/api/settings/basic-info/general-sections", generalSections);
   //------
   app.use("/api/settings/accesses/page-accesses", pageAccesses);
-  //------
-  app.use("/api/settings/users-info/members", members);
-  app.use("/api/settings/users-info/doctors", doctors);
-  app.use("/api/settings/users-info/section-experts", sectionExperts);
-  //------
 };
